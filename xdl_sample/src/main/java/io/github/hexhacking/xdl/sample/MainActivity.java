d57e517570848ee4baf087c04dd1e8b040f105e2
caikelun <caikelun@gmail.com>
2024-08-08 17:47:00 +0800

    Improve the UI display of sample app.

--- xdl_sample/src/main/java/io/github/hexhacking/xdl/sample/MainActivity.java
+++ xdl_sample/src/main/java/io/github/hexhacking/xdl/sample/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.hexhacking.xdl.sample;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,10 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onTestClick(View view) {
-        NativeSample.test();
+        findViewById(R.id.testButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                NativeSample.test();
+            }
+        });
     }
 }
